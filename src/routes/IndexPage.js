8e5139366d5a2418c233c01907deff6f243a135a
dofeel <375163888@qq.com>
2019-11-02 09:44:36 +0800

    初次提交

---
+++ src/routes/IndexPage.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { connect } from 'dva';
+import styles from './IndexPage.css';
+import { Tabs, Affix } from 'antd';
+
+const { TabPane } = Tabs;
+
+function callback(key) {
+  console.log(key);
+}
+function IndexPage() {
+  return (
+    <div className={styles.normal}>
+     <Affix offsetTop={10}>
+        
+     <Tabs defaultActiveKey="1" onChange={callback}>
+    <TabPane tab="Tab 1" key="1">
+      Content of Tab Pane 1
+    </TabPane>
+    <TabPane tab="Tab 2" key="2">
+      Content of Tab Pane 2
+    </TabPane>
+    <TabPane tab="Tab 3" key="3">
+      Content of Tab Pane 3
+    </TabPane>
+  </Tabs>
+  </Affix>
+
+      <h1 className={styles.title}>Yay! Welcome to dva!</h1>
+      <div className={styles.welcome} />
+      <ul className={styles.list}>
+        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
+        <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
+      </ul>
+    </div>
+  );
+}
+
+IndexPage.propTypes = {
+};
+
+export default connect()(IndexPage);
